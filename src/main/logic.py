514f5ca728af6e047987bace9eb52dc35815d7c3
piturhcp <gonzalocabad@live.com>
2019-09-03 23:40:28 -0300

    User interaction started, idea of empty sudoku solver started

--- src/main/logic.py
+++ src/main/logic.py
@@ -1,7 +1,7 @@
 import csv
 
-from .exception.exceptions import *
-from .solver import Solver
+from src.main.exception.exceptions import *
+from src.main.solver import Solver
 
 
 def parse_csv(csv_file):
@@ -45,11 +45,51 @@ def solve():
     except KeyboardInterrupt:
         parse_array_to_csv(resolved_matrix, "../../resources/save.csv")
 
+def solveConPath(path):
+    resolved_matrix = []
+    try:
+        list_matrix = parse_csv(path)
+        for matrix in list_matrix:
+            solver = Solver(matrix)
+            solver.solve()
+            resolved_matrix.append(solver.get_matrix())
+        parse_array_to_csv(resolved_matrix, "../../resources/ArchivoResuelto.csv")
+    except KeyboardInterrupt:
+        parse_array_to_csv(resolved_matrix, "../../resources/Parcial.csv")
+
+def solveEmptyPath():
+    resolved_matrix = []
+    try:
+        list_matrix = parse_csv("../../resources/emptySudoku.csv")
+        for matrix in list_matrix:
+            solver = Solver(matrix)
+            solver.solveEmpty()
+            resolved_matrix.append(solver.get_matrix())
+        parse_array_to_csv(resolved_matrix, "../../resources/ArchivoResuelto.csv")
+    except KeyboardInterrupt:
+        parse_array_to_csv(resolved_matrix, "../../resources/Parcial.csv")
+
 
 # TODO: Interaccion con usuario
 def sudoku_solve():
-    pass
-
+    print("¡Bienvenido al Sudoku Solver!".center(60, "="))
+    print("Por favor, ingrese el número correspondiente a la opción que desea realizar:")
+    while True:
+        choice1 = input("1) Resolver uno o varios tableros de sudoku en un archivo de formato CSV.\n2) Continuar una ejecución parcial\n3) Datos del grupo.\n4) Salir\n")
+        if choice1 == "1":
+            filePath = input("Por favor, complete el path del archivo que desea utilizar:\n")
+            solveConPath(filePath)
+            print("Finalizado.")
+            break
+        elif choice1 == "2":
+            print("elegiste 2")
+            solveEmptyPath()
+        elif choice1 == "3":
+            print("Sudoku solver realizado por Toloza, Tomas y Abad, Gonzalo.")
+        elif choice1 == "4" or choice1 == "salir":
+            exit()
+        else:
+            print("Por favor, introduzca una opción valida.")
 
 if __name__ == '__main__':
-    solve()
+    sudoku_solve()
--- src/main/solver.py
+++ src/main/solver.py
@@ -61,6 +61,33 @@ class Solver(object):
                     self.matrix[coor[0]][coor[1]] = 0
         return False
 
+    def solveEmpty(self):
+        # TODO: fix method. La clave esta en la parte de is_PLaceable!
+        """  Como debería funcionar esto:
+        #  1) Find all legal values of a given cell
+        #  2) For each legal value, Go recursively and try to solve the grid
+        Creo que el error está en que el programa funciona con una matriz interna que tiene como atributo de la clase. Deberíamos hacer que los metodos
+        que chequean (get_empty, is_in_row, is_in_col, is_in_box, is_placeable) reciban como parametro una matriz, y no trabajar con la interna (self.matrix).
+        Creo que la clave está en que al ejectuar el for y el is_placeable, cree una nueva matriz con ese valor o algo asi, para que cuando vuelva para atras haga otra matriz distinta o algo asi.
+        estoy cansado, voy a dormir, mañana la sigo jaja. De ultima esto de acá no le des pelota que despues lo sigo.
+        si lees este comentario hasta el final te mereces un buen nepe."""
+
+        x = 0
+        while x < 10:
+            coor = [0, 0]
+            if self.get_empty(coor) == [-1, -1]:
+                return True
+
+            for num in range(1, 10):
+                if self.is_placeable(coor[0], coor[1], num):
+                    self.matrix[coor[0]][coor[1]] = num
+                    if self.solve():
+                        x+=1
+                        print(self.get_matrix())
+                    else:
+                        self.matrix[coor[0]][coor[1]] = 0
+                return False
+
     def get_matrix(self):
         return self.matrix
 
