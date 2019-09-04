ef7067140967fa0fff3655b9e135da2e6e2c1013
tomastoloza <tomasezequieltoloza@gmail.com>
2019-09-04 08:42:44 -0300

    Refactor methods

--- src/main/logic.py
+++ src/main/logic.py
@@ -45,7 +45,8 @@ def solve():
     except KeyboardInterrupt:
         parse_array_to_csv(resolved_matrix, "../../resources/save.csv")
 
-def solveConPath(path):
+
+def solve_path(path):
     resolved_matrix = []
     try:
         list_matrix = parse_csv(path)
@@ -57,7 +58,8 @@ def solveConPath(path):
     except KeyboardInterrupt:
         parse_array_to_csv(resolved_matrix, "../../resources/Parcial.csv")
 
-def solveEmptyPath():
+
+def solve_empty():
     resolved_matrix = []
     try:
         list_matrix = parse_csv("../../resources/emptySudoku.csv")
@@ -75,21 +77,25 @@ def sudoku_solve():
     print("¡Bienvenido al Sudoku Solver!".center(60, "="))
     print("Por favor, ingrese el número correspondiente a la opción que desea realizar:")
     while True:
-        choice1 = input("1) Resolver uno o varios tableros de sudoku en un archivo de formato CSV.\n2) Continuar una ejecución parcial\n3) Datos del grupo.\n4) Salir\n")
+        choice1 = input("1) Resolver uno o varios tableros de sudoku en un archivo de formato CSV.\n"
+                        "2) Continuar una ejecución parcial\n"
+                        "3) Datos del grupo.\n"
+                        "4) Salir\n")
         if choice1 == "1":
-            filePath = input("Por favor, complete el path del archivo que desea utilizar:\n")
-            solveConPath(filePath)
+            file_path = input("Por favor, complete el path del archivo que desea utilizar:\n")
+            solve_path(file_path)
             print("Finalizado.")
             break
         elif choice1 == "2":
             print("elegiste 2")
-            solveEmptyPath()
+            solve_empty()
         elif choice1 == "3":
             print("Sudoku solver realizado por Toloza, Tomas y Abad, Gonzalo.")
         elif choice1 == "4" or choice1 == "salir":
             exit()
         else:
             print("Por favor, introduzca una opción valida.")
+
 
 if __name__ == '__main__':
     sudoku_solve()
